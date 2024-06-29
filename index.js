d9d7aca865f8e4841460a24f568ae2227c746df8
lewis <lewiskipkemoi53@gmail.com>
2024-06-29 13:50:51 +0300

    all

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName="bob";
+function uppercaseCustomerName(){
+    customerName=customerName.toUpperCase();
+    
+}
+function setBestCustomer(){
+    bestCustmer="not bob";
+}
+const leastFavoriteCustomer = 'cheruiyot';
+function changeLeastFavoriteCustomer() {
+    
+    leastFavoriteCustomer = 'cheruiyot';
+  }
+  console.log(customerName); 
+
+  upperCaseCustomerName();
+  console.log(customerName); 
+  
+  setBestCustomer();
+  console.log(bestCustomer); 
+  
+  overwriteBestCustomer();
+  console.log(bestCustomer); 
+
+  console.log(leastFavoriteCustomer); 
+  changeLeastFavoriteCustomer(); 
+    
